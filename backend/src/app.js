929d4b6aca2a65e5579e3ff9fc75c49f4dcd06b3
Souly1080 <soulaymaneakc22@gmail.com>
2025-05-11 01:01:40 +0200

    Update CORS configuration to be more permissive

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,13 +7,21 @@ const stripeRoutes = require('./routes/stripeRoutes');
 
 const app = express();
 
-// Middleware
+// Middleware - Configuration CORS très permissive pour le débogage
 app.use(cors({
-  origin: ['https://reviuonline.com', 'http://localhost:3000'], // Accepte le frontend déployé et localhost
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: '*', // Accepte toutes les origines
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   credentials: true
 }));
+
+// Ajout de headers supplémentaires pour résoudre les problèmes CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 app.use(express.json());
 
 // Routes
